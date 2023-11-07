22a98b56561f0838de22380fb5d27fd523df38b3
dustin1016 <106299264+dustin1016@users.noreply.github.com>
2023-11-07 12:21:07 +0800

    .

--- src/pages/Faculty.js
+++ src/pages/Faculty.js
@@ -1,21 +1,37 @@
 import React, {useState, useEffect} from "react";
 import departments from "./departments";
 
+const status = [
+  "in",
+  "out",
+  "on leave",
+  "on travel"
+];
 const Faculty = () => {
-
+    
     const [facultyList, setFacultyList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [hasData, setHasData] = useState(false);
     const [selectedDepartment, setSelectedDepartment] = useState('');
     const [selectedFaculty, setSelectedFaculty] = useState(null);
+    
     useEffect(()=>{
         if (!hasData){
             fetchList();   
         }
     },[]);
 
+    // useEffect(()=>{
+    //   if (selectedFaculty !== null){
+    //     //update the status of the selected faculty
+    //     const selectedId = selectedFaculty.faculty_idx;
+    //     const stat = facultyList.find(x=>x.faculty_idx === selectedId).status;
+    //     selectedFaculty.status = stat;
+    //   }
+    // },[facultyList]);
 
 
+    
     const fetchList = async()=> {
         try {
             const response = await fetch('http://localhost/qsrv/data.php/faculties');
@@ -65,11 +81,11 @@ const Faculty = () => {
       }
 
       const FacultyListCard = React.memo(({ id, name, img }) => {
-        // const imgpath = img !== null ? "http://localhost/qsrv/img/"+img : 'images/blank.webp';
+        const imgpath = img !== null ? "http://localhost/qsrv/img/"+img : 'images/blank.webp';
        
         return (
             <div onClick={()=>handleFacultyClick(id)} className=" border-b cursor-pointer hover:bg-slate-500 border-black w-full p-3 flex flex-row items-center">
-                {/* <img src={imgpath} height={50} width={50} className="rounded-full object-cover" /> */}
+                <img src={imgpath} height={50} width={50} className="rounded-full object-cover object-top" />
                 <p className="text-lg ml-3">{name}</p>
             </div>
         );
@@ -109,9 +125,107 @@ const Faculty = () => {
             )
       }
 
+      const updateFacultyStatus = async(status) => {
+        try {
+          const response = await fetch('http://localhost/qsrv/data.php/updateFacultyStatus',
+          {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: selectedFaculty?.faculty_idx, status: status})
+          });
+          const jsonData = await response.json();
+          
+         
+          let data = jsonData;
+          if (data.result) {
+            //update UI -> faculty status
+           
+            // selectedFaculty.status = status
+            setFacultyList(prevFacultyList => {
+              const updatedList = prevFacultyList.map(faculty => {
+                if (faculty.faculty_idx === selectedFaculty?.faculty_idx) {
+                  return { ...faculty, status: status };
+                }
+                return faculty;
+              });
+              return updatedList;
+            });
 
+            return true
+          } else {
+            return false;
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+
+      const handleStatusChange = async (newStatus) => {
+        // Call the updateFacultyStatus function with newStatus
+        if (await updateFacultyStatus(newStatus)){
+          setSelectedFaculty((prevSelectedFaculty) => ({
+            ...prevSelectedFaculty,
+            status: newStatus,
+          }));
+  
+        }
+
+      
+        
+      };
+
+      const StatusButton = ({ status, selectedStatus, onClick }) => {
+        const isActive = status === selectedStatus;
+        return (
+          <button
+            onClick={() => onClick(status)}
+            className={`uppercase text-xl p-3 border border-gray hover:border-black rounded-md ${
+              isActive ? "bg-green-600 text-black" : ""
+            }`}
+          >
+            {status}
+          </button>
+        );
+      };
    
-
+      function SelectedFaculty(){
+          
+          const imgpath = selectedFaculty !== null ? "http://localhost/qsrv/img/"+selectedFaculty?.pic_name : 'images/blank.webp';
+        return(
+          <div className="flex flex-row border border-slate-800 rounded-md p-4">
+            <div className="flex flex-col items-center justify-center">
+              <div className="h-72 w-72">
+                <img src={imgpath} className="object-cover w-full h-full object-top" />
+              </div>
+              <p className="text-start text-2xl">{selectedFaculty?.name}</p>
+            </div>
+            <div className="ml-4">
+                <div className="grid grid-cols-4 gap-4">
+                  {status.map((item, index)=>{
+                    // return(
+                    //   <button key={index} onClick={()=>{updateFacultyStatus(item)}} className={`uppercase text-xl p-3 border border-gray hover:border-black rounded-md
+                    //   ${selectedFaculty?.status===item && 'bg-green-600 text-black'}
+                    //   `}>
+                    //     {item}
+                    //   </button>
+                    // )
+                    return(
+                      <StatusButton
+                      key={index}
+                      status={item}
+                      selectedStatus={selectedFaculty?.status}
+                      onClick={handleStatusChange}
+                    />
+                    )
+                  })}
+                </div>
+            </div>
+          </div>
+        )
+      }
       return (
         <div className="p-1 relative ">
             
@@ -120,7 +234,9 @@ const Faculty = () => {
             <div className=" p-4">
                 <div className="flex flex-row">
                     <FacultyListBox />
-                    <p className="grow ml-3">{selectedFaculty?.name}</p>
+                    <div className="ml-3 grow">
+                      <SelectedFaculty />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/pages/SelectedFaculty.js
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from "react";
+const status = [
+    "in",
+    "out",
+    "on leave",
+    "on travel"
+  ];
+
+const SelectedFaculty=({selectedFaculty})=>{
+
+    const [facultyStatus, setFacultyStatus]=useState("out");
+
+
+    useEffect(()=>{
+       setFacultyStatus(selectedFaculty.status) 
+    },[]);
+
+    
+
+    const updateFacultyStatus = async(status) => {
+        try {
+          const response = await fetch('http://localhost/qsrv/data.php/updateFacultyStatus',
+          {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({id: selectedFaculty?.faculty_idx, status: status})
+          });
+          const jsonData = await response.json();
+          
+         
+          let data = jsonData;
+          if (data.result) {
+            //update UI -> faculty status
+            console.log(status)
+            selectedFaculty.status = status
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+
+}
+
+
+
+export default SelectedFaculty;
