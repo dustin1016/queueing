d769d1bf82579ae2fc8c8c243089664466571788
dustin1016 <106299264+dustin1016@users.noreply.github.com>
2023-11-21 09:03:19 +0800

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -78,7 +78,7 @@ const Dashboard = () => {
             </div>
             <div>
             <div className='flex flex-row mb-6 p-3 items-center justify-center ' >
-                <img src='images-logo.png' height={150} width={150} />
+                <img src='images/cba-logo.png' height={150} width={150} />
                 <h2 className='text-center lg:text-2xl xl:text-4xl font-bold ml-3'>College of Business and Accountancy</h2>
               </div>
 
